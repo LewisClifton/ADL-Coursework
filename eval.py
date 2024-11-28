cc330a8f4dcd7522d255a5936641d250fd292789
LewisClifton <lewisclifton2003@gmail.com>
2024-11-28 01:26:04 +0000

    fixed image saving

--- eval.py
+++ eval.py
@@ -92,7 +92,7 @@ def evaluate(model, test_loader, test_data, GT_fixations_dir, image_dir, num_sav
             preds[image_name] = pred_fixMap
             targets[image_name] = GT_fixMap
 
-            if map_idx < num_saved_images and device == 0:
+            if map_idx < num_saved_images:
 
                 # Concatenate all three images along the width (axis 1) and convert to PIL image
                 concatenated_image = np.concatenate((pred_fixMap * 255, GT_fixMap), axis=1)
